75bfe447b4dfc878301958879e909005e108817e
Anton Prozorov <zlobstermeister@gmail.com>
2014-06-09 17:48:10 +0400

    Player fragment

--- ZMPlayer2/src/main/java/zmplayer2/app/ui/views/PlayerFragment.java
+++ ZMPlayer2/src/main/java/zmplayer2/app/ui/views/PlayerFragment.java
@@ -37,7 +37,7 @@ public class PlayerFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_player, container, false);
         return rootView;
     }
 
